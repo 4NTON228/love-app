0a6c6e1bb73c53e44dbf8bb6805b92c05b77ecad
4NTON228 <57921244+4NTON228@users.noreply.github.com>
2026-02-14 01:33:37 +0300

    Упрощен service worker для скорости

--- public/sw.js
+++ public/sw.js
@@ -1,43 +1,7 @@
-const CACHE_NAME = 'love-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
+﻿self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response.status === 200) {
-          const responseClone = response.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => cache.put(event.request, responseClone));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
+  event.respondWith(fetch(event.request))
+})
